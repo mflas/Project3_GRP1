d3b1d563ef1bcc1ada53dc7a21cebb2f0ecb85c9
Anush De Costa <67308030+AnushDeCosta@users.noreply.github.com>
2023-03-19 21:38:00 +1100

    updated code

--- static/js/app.js
+++ static/js/app.js
@@ -1,67 +1,66 @@
-// Set the URL for the JSON data source
-var url = "static/bionet2.json";
+$(document).ready(function() {
+    // Use ajax function from jQuery library to retrieve data as JSON from server endpoint /api/bionet 
+    $.ajax({
+        url: '/api/bionet',
+        dataType: 'json',
+        // Specify a callback function for the successful retrieval of the data and
+        // Use d3 to select the dropdown element with the id 'selDataset'
+        success: function(data) {
+            var dropdown = d3.select("#selDataset");
+            // Extract unique vernacular names from the data and sort the vernacular names alphabetically
+            var vernacularNames = [...new Set(data.map(record => record.vernacularName))];
+            vernacularNames.sort();
+            // Iterate over each vernacular name and append an option element to the dropdown
+            vernacularNames.forEach(function(name) {
+                dropdown.append("option").text(name).property("value", name);
+            });
 
-// Use D3 to fetch the JSON data and log it to the console
-d3.json(url).then(function(data) {
-    console.log(data);
+            // Function to display the total number of sightings of the selected animal
+            function displayTotalSightings(animalName, data) {
+                // Filter the data to get records only for the selected animal
+                var selectedAnimalData = data.filter(function(record) {
+                    return record.vernacularName === animalName;
+                });
+                // Get the count of the filtered records
+                var totalSightings = selectedAnimalData.length;
+                // Display the count in the element with id 'totalSightings'
+                if (animalName) {
+                    d3.select("#totalSightings").html("Total number of sightings: <b>" + totalSightings + "</b>");
+                }
+            }
+
+            // Event listener for when a new animal is selected from the dropdown
+            function optionChanged(animalName) {
+                // Use ajax function from jQuery library to retrieve data as JSON from server endpoint /api/bionet 
+                $.ajax({
+                    url: '/api/bionet',
+                    dataType: 'json',
+                    success: function(data) {
+                        // Call the displayTotalSightings function to display the total number of sightings of the selected animal
+                        displayTotalSightings(animalName, data);
+                        // Call the buildPlot function to build the plot for the selected animal
+                        buildPlot(animalName, data);
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        console.log('Error retrieving data from server: ' + textStatus);
+                    }
+                });
+            }
+
+            // Build the plot after the document has loaded and the necessary elements have been created
+            buildPlot();
+
+            // Event listener for when a new animal is selected from the dropdown
+            dropdown.on("change", function() {
+                // Get the value of the selected dropdown option
+                var selectedAnimal = d3.select(this).property("value");
+                // Call the optionChanged function with the selected animal's vernacular name
+                optionChanged(selectedAnimal);
+            });
+        },
+        // Specify a callback function to handle any errors during the data retrieval process
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log('Error retrieving data from server: ' + textStatus);
+        }
+    });
 });
-
-// Define function to initialize the dropdown menu
-function init() {
-  // Select the dropdown menu element
-  var selector = d3.select("#selDataset");
-
-  // Use D3 to get the list of sample names
-  d3.json(url).then((data) => {
-    var sampleNames = Array.from(new Set(data.value.map(d => d.vernacularName))); // Only unique values in vernacularName
-    // Loop through the sample names and append an options
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
-
-    // Use the first sample from the list to build the initial plots
-    var firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
-  });
-}
-
-// Initialize the dashboard
-init();
-
-// Define function to update charts and metadata
-function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  buildMetadata(newSample);
-  buildCharts(newSample);
-}
-
-// Define function to build metadata panel 
-function buildMetadata(sample) {
-  // Use D3 to get the metadata for all samples
-  d3.json(url).then((data) => {
-    var metadata = data.value;
-
-    // Filter the metadata for the object with the desired sample number
-    var resultArray = metadata.filter(sampleObj => sampleObj.vernacularName == sample);
-    var result = resultArray[0];
-  
-    // Select the metadata panel element
-    var PANEL = d3.select("#sample-metadata");
-
-    // Clear the previous contents of the metadata panel
-    PANEL.html("");
-
-    // Loop through each key-value pair in the result object and append the metadata panel
-    Object.entries(result).forEach(([key, value]) => {
-      if (key !== "class" && key !== "basisOfRecord" && key !== "sex" && key !== "vernacularName") {
-        PANEL.append("h6").text(`${key}: ${value}`);
-      }
-    });
-  });
-}
-
-
